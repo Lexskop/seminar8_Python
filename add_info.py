9c11834db99011816daf18d39b774b1b14832064
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-12-22 10:21:38 +0300

    внесены корректировки в файл add_info, добавлено отчество

--- add_info.py
+++ add_info.py
@@ -2,56 +2,79 @@ import csv
 
 def add_info(file):
     last_name = input('Введите фамилию ученика: ').capitalize()
-    last_name = last_name.upper()
     first_name = input('Введите имя ученика: ').capitalize()
-    first_name = first_name.upper()
+    middle_name = input('Введите отчество ученика: ').capitalize()
     num_class = input('Введите класс ученика: ').lower()
     with open(file, 'a', encoding = 'UTF-8', newline='') as data:
-        fields = ['Фамилия', 'Имя', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
+        fields = ['Фамилия', 'Имя', 'Отчество', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
                 'Биология', 'Химия', 'История', 'География', 'Физика']
         writer = csv.DictWriter(data, fieldnames= fields)
-        writer.writerow({'Фамилия': last_name, 'Имя': first_name, 'Класс': num_class})
+        writer.writerow({'Фамилия': last_name, 'Имя': first_name, 'Отчество': middle_name, 'Класс': num_class})
 
         
-def add_grades(file):
+def add_grades(file,word):
     subject = input('Введите название предмета: ').capitalize()
     grade = input('Введите оценку: ')
     number = int(input('Введите порядковый номер, указанный выше перед фамилией ученика: '))
     my_dict = []
     with open(file, 'r', encoding = 'UTF-8', newline='') as data:
         for row in csv.reader(data):
-            my_dict.append({'Фамилия': row[0], 'Имя': row[1], 'Класс': row[2], 'Литература': row[3], 'Русский язык': row[4], 'Математика': row[5],\
-                      'Физкультура': row[6], 'Информатика': row[7], 'Иностранный язык': row[8], 'Биология': row[9], 'Химия': row[10], 'История': row[11],\
-                          'География': row[12], 'Физика': row[13]})
-    my_dict[number][subject] += f'{grade} '
-    with open(file, 'w', encoding = 'UTF-8', newline='') as data:
-        fields = ['Фамилия', 'Имя', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
-                'Биология', 'Химия', 'История', 'География', 'Физика']
-        writer = csv.DictWriter(data, fieldnames= fields)
-        writer.writerows(my_dict)
+            my_dict.append({'Фамилия': row[0], 'Имя': row[1], 'Отчество': row[2], 'Класс': row[3], 'Литература': row[4], 'Русский язык': row[5], 'Математика': row[6],\
+                      'Физкультура': row[7], 'Информатика': row[8], 'Иностранный язык': row[9], 'Биология': row[10], 'Химия': row[11], 'История': row[12],\
+                          'География': row[13], 'Физика': row[14]})
+    with open(file, 'r', encoding = 'UTF-8', newline='') as data:
+        count = 0
+        total = 0
+        file_reader = csv.DictReader(data, delimiter=',')
+        for line in file_reader:
+            total +=1
+            for i in ['Фамилия','Имя','Отчество','Класс']:
+                if word == line[i]:
+                    count += 1
+                if count == number:
+                    my_dict[total][subject] += f'{grade} '
+                    with open(file, 'w', encoding = 'UTF-8', newline='') as data:
+                        fields = ['Фамилия', 'Имя', 'Отчество', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
+                                'Биология', 'Химия', 'История', 'География', 'Физика']
+                        writer = csv.DictWriter(data, fieldnames= fields)
+                        writer.writerows(my_dict)
+                        return
     
 
-def change_info(file):
+def change_info(file,word):
     print('Введите цифру, соответствующую типу информации, которую нужно изменить:')
-    type_info = int(input('1 - фамилия\n2 - имя\n3 - класс\n'))
+    type_info = int(input('1 - фамилия\n2 - имя\n3 - отчество\n4 - класс\n'))
     column = ''
     if type_info == 1:
         column = 'Фамилия'
     elif type_info == 2:
         column = 'Имя'
+    elif type_info == 3:
+        column = 'Отчество'
     else:
         column = 'Класс'
     number = int(input('Введите порядковый номер, указанный выше перед фамилией ученика: '))
     my_dict = []
     with open(file, 'r', encoding = 'UTF-8', newline='') as data:
         for row in csv.reader(data):
-            my_dict.append({'Фамилия': row[0], 'Имя': row[1], 'Класс': row[2], 'Литература': row[3], 'Русский язык': row[4], 'Математика': row[5],\
-                      'Физкультура': row[6], 'Информатика': row[7], 'Иностранный язык': row[8], 'Биология': row[9], 'Химия': row[10], 'История': row[11],\
-                          'География': row[12], 'Физика': row[13]})
+            my_dict.append({'Фамилия': row[0], 'Имя': row[1], 'Отчество': row[2], 'Класс': row[3], 'Литература': row[4], 'Русский язык': row[5], 'Математика': row[6],\
+                      'Физкультура': row[7], 'Информатика': row[8], 'Иностранный язык': row[9], 'Биология': row[10], 'Химия': row[11], 'История': row[12],\
+                          'География': row[13], 'Физика': row[14]})
     new_info = input('Введите новую информацию: ')
-    my_dict[number][column] = new_info
-    with open(file, 'w', encoding = 'UTF-8', newline='') as data:
-        fields = ['Фамилия', 'Имя', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
-                'Биология', 'Химия', 'История', 'География', 'Физика']
-        writer = csv.DictWriter(data, fieldnames= fields)
-        writer.writerows(my_dict)
+    with open(file, 'r', encoding = 'UTF-8', newline='') as data:
+        count = 0
+        total = 0
+        file_reader = csv.DictReader(data, delimiter=',')
+        for line in file_reader:
+            total +=1
+            for i in ['Фамилия','Имя','Отчество','Класс']:
+                if word == line[i]:
+                    count += 1
+                if count == number:
+                    my_dict[total][column] = new_info
+                    with open(file, 'w', encoding = 'UTF-8', newline='') as data:
+                        fields = ['Фамилия', 'Имя', 'Отчество', 'Класс', 'Литература', 'Русский язык', 'Математика', 'Физкультура', 'Информатика', 'Иностранный язык',\
+                                'Биология', 'Химия', 'История', 'География', 'Физика']
+                        writer = csv.DictWriter(data, fieldnames= fields)
+                        writer.writerows(my_dict)
+                        return
